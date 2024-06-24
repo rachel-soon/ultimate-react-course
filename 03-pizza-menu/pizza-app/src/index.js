850165c40517c1605526271f3285c66c299d5365
rachel-soon <rachel_may0106@hotmail.com>
2024-06-24 22:30:19 +0800

    feat: react fragments

--- 03-pizza-menu/pizza-app/src/index.js
+++ 03-pizza-menu/pizza-app/src/index.js
@@ -77,17 +77,24 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2> Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={JSON.stringify(pizza)} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={JSON.stringify(pizza)} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p> We're still working on our menu. Please come back later</p>
       )}
