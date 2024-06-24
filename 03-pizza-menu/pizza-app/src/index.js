d1a215662a8149bd9d0e18f0b10566a5dd7fd3e7
rachel-soon <rachel_may0106@hotmail.com>
2024-06-24 22:05:12 +0800

    feat: create Order component

--- 03-pizza-menu/pizza-app/src/index.js
+++ 03-pizza-menu/pizza-app/src/index.js
@@ -128,9 +128,6 @@ function Footer() {
   const openHour = 8;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  // if (hour >= openHour && hour <= closeHour) {
-  //   alert("We're currently open");
-  // } else alert("Sorry, we're closed");
 
   if (!isOpen) return;
   <p>
@@ -140,16 +137,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online!</p>
-          <button className="btn"> Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online!
+      </p>
+      <button className="btn"> Order</button>
+    </div>
   );
 }
 
