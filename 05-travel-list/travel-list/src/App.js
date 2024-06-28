5b8c486378ac9838e7e01565eed3dff4e6bc29bb
rachel-soon <rachel_may0106@hotmail.com>
2024-06-28 21:11:29 +0800

    feat: rendering the list and implementing event listeners

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,3 +1,10 @@
+import { useState } from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 5, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -18,20 +25,56 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <>
-      <div className="add-form">
-        <h3> What do you need for your 😍 trip? </h3>
-      </div>
-    </>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3> What do you need for your 😍 trip? </h3>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
   return (
-    <>
-      <div className="list">LIST</div>
-    </>
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 
@@ -39,7 +82,7 @@ function Stats() {
   return (
     <>
       <footer className="stats">
-        👜 You have X items on your list, and you already packed X (%X)
+        👜 You have X items on your list, and you already packed X (X%)
       </footer>
     </>
   );
