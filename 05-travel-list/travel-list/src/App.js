1723cc0eae2dabe342cd4ad06842fba2d6862ae0
rachelmay-kiratech <rachel@kiratech.com.au>
2024-07-04 21:16:34 +0800

    feat: implement stats

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -32,7 +32,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -119,11 +119,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>🫸 Start adding some items to your packing list 🫷</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <>
       <footer className="stats">
-        👜 You have X items on your list, and you already packed X (X%)
+        <em>
+          {percentage === 100
+            ? "You got everything! Ready to go ✈️"
+            : `👜 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+        </em>
       </footer>
     </>
   );
